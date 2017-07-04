a3a608e911cdeb9ea36d4a072f78dadc1044728d
DanielSzallasi <danielszallasi@gmail.com>
2017-07-04 10:27:56 +0200

    Function joke

--- gittie.py
+++ gittie.py
@@ -1,2 +1,6 @@
 def introduce():
     print("Hello I'm Gittie!")
+
+def joke():
+    print("It's called the Grandfather Paradox. The reason why a time machine is impossible is 'cause if you had a time machine, you could go back in time, and kill your grandfather before your father was ever conceived, thus you could have never existed to make that time machine. I'm like what kind of asshole wants to kill his grandfather.")
+    
